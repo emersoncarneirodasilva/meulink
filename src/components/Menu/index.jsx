828094708a8198f83bccd9cc16747fa5a615212b
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-04-17 01:08:23 -0300

    Atual

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 function Menu() {
     return (
         <div className="menu">
-            <a className="social" href="https://youtube.com/c/sujeitoprogramador" target="_blanktarget">
+            <a className="social" href="https://youtube.com/c/sujeitoprogramador">
                 <BsYoutube size={24} />
             </a>
-            <a className="social" href="https://instagram.com/sujeitoprogramador" target="_blanktarget">
+            <a className="social" href="https://instagram.com/sujeitoprogramador">
                 <BsInstagram size={24} />
             </a>
             <Link className="menu-item" to="/links">
